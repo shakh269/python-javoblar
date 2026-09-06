2991401e3bb9289a55324df7dc643786efbf4e5a
shakh269 <shahriyorsoatillayev@gmail.com>
2026-09-06 15:44:42 +0500

    Create Funksiyalar.py

---
+++ Funksiyalar.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  6 07:08:19 2026
+
+@author: HP
+"""
+
+# def salom_ber():
+#     """salom beruvchi funksiya bu"""
+#     print('hello, good day')
+    
+# salom_ber()
+    
+# def salom_ber(name):
+#     print(f"Hello, mister {name.title()}!")
+# salom_ber('john')
+    
+# print(print.__doc__)
+
+# def name_surname (name, surname):
+#     print(f"user name: {name.title()}\n"
+#           f"user surname:{surname.title()}")
+# print(name_surname('mike' , 'tyson'))
+    
+
+# def age_counter (name, born_year):
+#     print(f"{name.title()}ning yoshi {2026-born_year} da")
+# age_counter('nike', 2000) 
+    
+    
+#     Foydalanuvchi ismi va yoshini so'rab, uning tug'ilgan 
+# yilini hisoblaydigan funksiya yozing.
+# Foydalanuvchidan son olib, uning kvadrati va
+#  kubini konsolga chiqaruvchi funksiya yozing.
+# Foydalanuvchidan son olib, son juft yoki 
+# toqligini konsolga chiqaruvchi funksiya yozing.
+# Foydalanuvchidan ikkita son olib, 
+# ulardan kattasini konsolga chiqaruvchi y
+# funksiya yozing. Agar sonlar teng bo'lsa 
+# "Sonlar teng" degan xabarni chiqaring.
+# Foydalanuvchidan x va y sonlarini olib, 
+# ni konsolga chiqaruvchi funksiya yozing.
+# Yuqoridagi funksiyada y uchun 2 standart qiymatini bering.
+# Foydalanuvchidan son qabul qilib, sonni 2 dan 10 gacha
+#  bo'lgan sonlarga qoldiqsiz bo'linishini tekshiruvchi 
+#  funksiya yozing. Natijalarni konsolga chiqaring.
+    
+
+# def name_borname(name, bornyear):
+#     print(f"{name.title()}ning yoshi {2026-bornyear}da")
+# # name=input('ismingizni kiriting')
+# # bornyear=input('tugilgan yilingizni kiriting') 
+# name_borname('nike',2000)  
+    
+
+# def comparison(number1,number2):
+#     if number1>number2:
+#         print(f"{number1} katta son")
+#     else:
+#         print(f"{number2} katta son")
+# comparison(34,40)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
